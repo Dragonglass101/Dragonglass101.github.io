efb2767a196df08f057b9eddee0ac94d0bbae31d
Anurag Singh Naruka <Anuragsinghnaruka@gmail.com>
2022-09-03 15:27:18 +0530

    Changed scrollbar styling

--- js/myProjects.js
+++ js/myProjects.js
@@ -1,128 +1,35 @@
-const projects = [
-  {
-    img: '../imgs/taste-food.PNG',
-    title: 'Taste Food',
-    description:
-      'Taste food is a web application based on an external food API, showing data about foods and we used Used involvement API to record the different user interactions (likes, comments).',
-    live: 'https://bondok6.github.io/Taste-Food/',
-    github: 'https://github.com/Bondok6/taste-food/',
-  },
-  {
-    img: '../imgs/todo-list.PNG',
-    title: 'Todo List',
-    description:
-      'In this project, I built a simple To-Do App for managing the daily tasks and to keep track on tasks during the day.',
-    live: 'https://bondok6.github.io/ToDo-List/',
-    github: 'https://github.com/Bondok6/ToDo-List/',
-  },
-  {
-    img: '../imgs/space-travelers.PNG',
-    title: 'Space Travelers',
-    description:
-      'A web app that provides commercial and scientific space travel services. The app allows users to book rockets and join selected space missions using real live data from the SpaceX API.',
-    live: 'https://space-travelers.herokuapp.com/',
-    github: 'https://github.com/Bondok6/space-travelers',
-  },
-  {
-    img: '../imgs/natours.PNG',
-    title: 'Natours',
-    live: 'https://bondok6.github.io/Natours/',
-    github: 'https://github.com/Bondok6/Natours',
-  },
-  {
-    img: '../imgs/math-magicians.PNG',
-    title: 'Math Magicians',
-    description:
-      'A website for all fans of mathematics. It is a React.js project that allows users to make simple calculations and read a random math-related quote.',
-    live: 'https://bondok6.github.io/math-magicians/',
-    github: 'https://github.com/Bondok6/math-magicians',
-  },
-  {
-    img: '../imgs/Leaderboard.PNG',
-    title: 'Leaderboard',
-    description:
-      'A website displays scores submitted by different players. All data is preserved thanks to the external Leaderboard API.',
-    live: 'https://bondok6.github.io/Leaderboard/',
-    github: 'https://github.com/Bondok6/Leaderboard/',
-  },
-  {
-    img: '../imgs/green-travel.PNG',
-    title: 'Green Travel',
-    description:
-      'This is a website for tourism and traveling around the world, exploring the world, and offering exciting tours for adventurers.',
-    live: 'https://bondok6.github.io/Green-Travel/',
-    github: 'https://github.com/Bondok6/Green-Travel',
-  },
-  {
-    img: '../imgs/awesome-books.PNG',
-    title: 'Awesome Books',
-    description:
-      'In this project, I built a basic website that allows users to add/remove books from a book list.',
-    live: 'https://bondok6.github.io/Awesome-Books/',
-    github: 'https://github.com/Bondok6/Awesome-Books',
-  },
-  {
-    img: '../imgs/countries.PNG',
-    title: 'Countries',
-    description:
-      'search for any country that you want to know information about. Build with RestCountry API, javaScript, CSS, HTML',
-    live: 'https://bondok6.github.io/countries/',
-    github: 'https://github.com/Bondok6/countries/',
-  },
-  {
-    img: '../imgs/mapty-app.PNG',
-    title: 'Mapty App',
-    description:
-      'In this app, you can log your running or cycling workouts with location,distance, time, and steps/minute. You can also see all your workouts on a map.',
-    live: 'https://bondok6.github.io/Mapty-app/',
-    github: 'https://github.com/Bondok6/Mapty-app',
-  },
-  {
-    img: '../imgs/pigGame.PNG',
-    title: 'Pig Game',
-    description:
-      'Dice Game. Each turn, a player repeatedly rolls a die until either a 1 is rolled or the player decides to "hold". Go to Github to read more...',
-    live: 'https://bondok6.github.io/Pig-Game/',
-    github: 'https://github.com/Bondok6/Pig-Game',
-  },
-  {
-    img: '../imgs/Bankist.PNG',
-    title: 'Bankist',
-    description:
-      'A beautiful website with a lot of different components and visual effects that require some pretty advanced DOM manipulation techniques.',
-    live: 'https://bondok6.github.io/Bankist/',
-    github: 'https://github.com/Bondok6/Bankist',
-  },
-  {
-    img: '../imgs/expense-tracker.PNG',
-    title: 'Expense Tracker',
-    description:
-      'App helps you manage your money on the go. By understanding what you spend money on and how much you spend, you can see exactly where your cash is going.',
-    live: 'https://bondok6.github.io/Expense-tracker-app/',
-    github: 'https://github.com/Bondok6/Expense-tracker-app',
-  },
-  {
-    img: '../imgs/zenva.png',
-    title: 'Zenva',
-    description:
-      'A website for Zenva, a company that provides a variety of services to the people of the world.',
-    live: 'https://bondok6.github.io/Zenva/',
-    github: 'https://github.com/Bondok6/ZENVA',
-  },
-  {
-    img: '../imgs/qualby.jpg',
-    title: 'Qualby Etmaa`n',
-    description:
-      'Qualby_Etmaa`n, A website to display episodes of a famous Arabic program shown on YouTube',
-    live: 'https://qalby-etma2n.herokuapp.com/',
-    github: 'https://github.com/Bondok6/qalby_etmaa-n',
-  },
-  {
-    img: '../imgs/guessGame.PNG',
-    title: 'Guess My Number',
-    description:
-      'Simple Game, Try to guess a number from 1 to 20 in the fewest number of attempts possible',
-    live: 'https://bondok6.github.io/Guess-My-Number/',
-    github: 'https://github.com/Bondok6/Guess-My-Number',
-  },
-];
+const projects = [{
+        img: "../imgs/Devilslist.png",
+        title: "Decentralized Angellist",
+        tech_stack: "Tezos, IPFS, React JS",
+        date: "July 21, 2022",
+        description: "DevilsList is an end-to-end platform where investors may invest in businesses using tez/USDT and sign SAFT/SAFEs directly on the site. When a round closes, this website keeps track of the cap-table, and entrepreneurs may pay their workers and vendors in tez/USDT",
+        live: "https://decentralizedangellist.web.app/",
+        github: "https://github.com/Dragonglass101/Decentralized-AngelList",
+    },
+    {
+        img: "../imgs/cuphead.png",
+        title: "Cuphead Clone",
+        tech_stack: "SDL Graphics Library",
+        date: "November 19, 2021",
+        description: "",
+        live: "",
+        github: "https://github.com/Dragonglass101/CPP-Project",
+    }, {
+        img: "../imgs/critical-point-visualization.png",
+        title: "Betti Number Calculation",
+        tech_stack: "Three JS",
+        date: "April 24, 2022",
+        description: "This is a visualization of critical points of a smooth manifold calculated using Morse Theory in order to find Betti numbers of a given surface",
+        live: "https://dragonglass101.github.io/Critical-Point-Visualization/",
+        github: "https://github.com/Dragonglass101/Critical-Point-Visualization",
+    }, {
+        img: "../imgs/sportify.png",
+        title: "Sportify",
+        tech_stack: "Firebase, React JS",
+        date: "March 21, 2022",
+        description: "A web application to manage sports tournaments and show live scores of different events. Was used for IIITB Sports Fest, Spandan. It provides the following functionalities: Users can view live scores, Admins can create and manage fixtures, Admins can update the score board",
+        live: "https://sportify-8829a.web.app/",
+        github: "https://github.com/Bondok6/Reservify",
+    },
+]
